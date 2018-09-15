d85a166640ea52ae68111c5ecfc74207070a7e22
robdjohns <robdjohns@gmail.com>
2018-09-15 17:56:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function happyHolidaysTo(name) {
   }
   
   function holidayCountdown (days, holiday) {
-    return `It's ${days} until ${holiday}!`
+    return `It's ${holiday} until ${days}!`
   }
